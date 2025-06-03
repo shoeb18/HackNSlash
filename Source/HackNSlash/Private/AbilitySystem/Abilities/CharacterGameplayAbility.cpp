c89a26fe2366db97e9e779f8d11adde916422b21
shoeb <shoebkhan0197@gmail.com>
2025-06-03 14:35:11 +0530

    setup ability activation policy

---
+++ Source/HackNSlash/Private/AbilitySystem/Abilities/CharacterGameplayAbility.cpp
@@ -0,0 +1,31 @@
+// CopyNinja All Rights Reserved
+
+
+#include "AbilitySystem/Abilities/CharacterGameplayAbility.h"
+#include "AbilitySystem/CharacterAbilitySystemComponent.h"
+
+void UCharacterGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	if (AbilityActivationPolicy == ECharacterAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle, false);
+		}
+	}
+}
+
+void UCharacterGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if (AbilityActivationPolicy == ECharacterAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
---
+++ Source/HackNSlash/Public/AbilitySystem/Abilities/CharacterGameplayAbility.h
@@ -0,0 +1,38 @@
+// CopyNinja All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "CharacterGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class ECharacterAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ * 
+ */
+UCLASS()
+class HACKNSLASH_API UCharacterGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+
+protected:
+
+	//~ Begin UGameplayAbility Interface.
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec);
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+	//~ End UGameplayAbility Interface.
+
+
+
+	/** The activation policy for this ability */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterAbility")
+	ECharacterAbilityActivationPolicy AbilityActivationPolicy = ECharacterAbilityActivationPolicy::OnTriggered;
+};
