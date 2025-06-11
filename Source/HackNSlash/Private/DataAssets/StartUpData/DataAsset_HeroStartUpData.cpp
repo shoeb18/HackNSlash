019d5ea5c57e00c297ea43d2554a81b895de2902
shoeb <shoebkhan0197@gmail.com>
2025-06-11 16:10:42 +0530

    Created struct for Abilities to grant in startup data

--- Source/HackNSlash/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/HackNSlash/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,30 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/CharacterGameplayAbility.h"
+#include "AbilitySystem/CharacterAbilitySystemComponent.h"
 
+bool FHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UCharacterAbilitySystemComponent* InCharacterASC, int32 ApplyLevel) const
+{
+	Super::GiveToAbilitySystemComponent(InCharacterASC, ApplyLevel);
+
+	for (const FHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+	{
+		if (!AbilitySet.IsValid())
+		{
+			continue;
+		}
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InCharacterASC->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+		InCharacterASC->GiveAbility(AbilitySpec);
+	}
+}
--- Source/HackNSlash/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
+++ Source/HackNSlash/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
@@ -4,8 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_HeroStartUpData.generated.h"
 
+class UCharacterGameplayAbility;
+
+USTRUCT(BlueprintType)
+struct FHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
+	TSubclassOf<UCharacterGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const;
+};
 /**
  * 
  */
@@ -13,5 +29,13 @@ UCLASS()
 class HACKNSLASH_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void GiveToAbilitySystemComponent(class UCharacterAbilitySystemComponent* InCharacterASC, int32 ApplyLevel = 1) const override;
+
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (TitleProperty = "InputTag"))
+	TArray<FHeroAbilitySet> HeroStartUpAbilitySets;
 	
 };
