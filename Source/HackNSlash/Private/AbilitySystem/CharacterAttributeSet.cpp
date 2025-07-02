357f5e61abbd7842c76c4dc7dab42d2966117ff0
shoeb <shoebkhan0197@gmail.com>
2025-07-02 15:55:55 +0530

    Setup Initial Character Attributes

--- Source/HackNSlash/Private/AbilitySystem/CharacterAttributeSet.cpp
+++ Source/HackNSlash/Private/AbilitySystem/CharacterAttributeSet.cpp
@@ -3,3 +3,12 @@
 
 #include "AbilitySystem/CharacterAttributeSet.h"
 
+UCharacterAttributeSet::UCharacterAttributeSet()
+{
+	InitCurrentHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitCurrentRage(0.0f);
+	InitMaxRage(100.0f);
+	InitAttackPower(10.0f);
+	InitDefensePower(5.0f);
+}
--- Source/HackNSlash/Public/AbilitySystem/CharacterAttributeSet.h
+++ Source/HackNSlash/Public/AbilitySystem/CharacterAttributeSet.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/CharacterAbilitySystemComponent.h"
 #include "CharacterAttributeSet.generated.h"
+
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +22,33 @@ class HACKNSLASH_API UCharacterAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+
+	UCharacterAttributeSet();
+
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, CurrentHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, CurrentRage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, MaxRage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, AttackPower);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, DefensePower);
+
 };
